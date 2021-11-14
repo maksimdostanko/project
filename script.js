b0137dfb2c18210c36d5322d0ab0a5c0fd4b4fe9
Ivan <example@gmail.com>
2021-11-14 13:23:31 +0300

    Loops

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
-"use strict";
+/* Задание на урок:
 
-// const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false 
-// };
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
 
-// const a = prompt('Один из последних просмотренных фильмов', ''),
-//       b = prompt('На сколько оцениете его?', ''),
-//       c = prompt('Один из последних просмотренных фильмов', ''),
-//       d = prompt('На сколько оцениете его?', '');
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+4) Потренироваться и переписать цикл еще двумя способами*/
 
-// console.log(personalMovieDB);
+'use strict';
 
-if(4 == 4) {
-    console.log('ОК!');
-} else {
-     console.log('Error!');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', ''); 
 
-
-
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Много');
-// } else {
-//     console.log('Ok'); 
-// }
-
-// (num === 50) ? console.log('Ok') : console.log('Error');
-
-
-
-const num = 50;
-switch (num) {
-    case 49:
-        console.log('Неверно');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно много фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
