4327dcb4687b80de99da019439dac051875ae774
Ivan <example@gmail.com>
2021-11-14 08:32:21 +0300

    If and Else

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-// "use strict";
+"use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false 
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false 
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов', ''),
+//       b = prompt('На сколько оцениете его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов', ''),
+//       d = prompt('На сколько оцениете его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+if(4 == 4) {
+    console.log('ОК!');
+} else {
+     console.log('Error!');
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцениете его?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцениете его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok'); 
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+
+const num = 50;
+switch (num) {
+    case 49:
+        console.log('Неверно');
+}
